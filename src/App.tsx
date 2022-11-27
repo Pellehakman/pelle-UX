f58651ae352a02c6b319bb45de699b029a98fa22
Pellehakman <perhakman@outlook.com>
2022-11-27 11:53:10 +0100

    1

--- src/App.tsx
+++ src/App.tsx
@@ -6,55 +6,56 @@ import { Years } from './models/data'
 import logo from './assets/Nobel_logo.svg'
 import './styles/variables.scss';
 
-// import json_laureates from './data/json_laureates.json'
-// import json_award from './data/json_award.json'
+import json_laureates from './data/json_laureates.json'
+import json_award from './data/json_award.json'
 
 function App() {
 
-//   const [obj, setObj] = useState<Years[]>([])
-//   const [whatYear, setWhatYear] = useState('1901')
+  const [obj, setObj] = useState<Years[]>([])
+  const [whatYear, setWhatYear] = useState('1901')
 
-//   const award = json_award
-//   const laureates = json_laureates
- 
-//  console.log(award)
-
-
-
-
-//   let yearWinners = award.filter(function (yearWinner) {
-//     return yearWinner.awardYear === `${whatYear}`;
-//   });
-
-// let catagoryWinners = yearWinners.map(f => f.category.en)
-
-// console.log(...catagoryWinners)
-//   const displayYears = obj.map((f) => <option key={f.year}>{f.year}</option>)
+  const award = json_award
+  const laureates = json_laureates
  
 
 
 
+
+
+  let yearWinners = award.filter(function (yearWinner) {
+    return yearWinner.awardYear === `${whatYear}`;
+  });
+
+let catagoryWinners = yearWinners.map(f => f.category.en)
+
+console.log(...catagoryWinners)
+
+  const displayYears = obj.map((f) => <option key={f.year}>{f.year}</option>)
+ 
+  console.log(displayYears)
+
+
  
 
 
 
  
 
-// const createYears = () => {
-//   let arr: Years[] = []
-//   setObj(arr)
-//   for (let i = 1901; i < 2020; i++){
-//     let years = {
-//       year: i
-//     }
-//     arr.push(years)
-//   }
-// }
+const createYears = () => {
+  let arr: Years[] = []
+  setObj(arr)
+  for (let i = 1901; i < 2020; i++){
+    let years = {
+      year: i
+    }
+    arr.push(years)
+  }
+}
 
 
-// useEffect(() => {
-//   createYears()
-// }, [])
+useEffect(() => {
+  createYears()
+}, [])
 
 const [toggleState, setToggleState] = useState(1);
 
@@ -77,16 +78,29 @@ const toggleTab = (index: SetStateAction<number>) => {
           <p className='info-p'>In this app you can see data in different diagrams <br/> over the years. </p>
 
           <div className='info-select'>
+            <div className='info-q1'>
             <h3>What do you want to know?</h3>
             <select className='select'>
               <option>Who won?</option>
             </select>
+            </div>
+
+
+            <div className='info-q1'>
+            <h3>What Year?</h3>
+            <select className='select'>
+              {displayYears}
+            </select>
+            </div>
+            
+
+           
           </div>
           
 
         </div>
         <div className="year-container">
-          <h1>1922</h1>       
+          <h1>1940</h1>       
         </div>
 
       </div>
